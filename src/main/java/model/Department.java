1a8ac0aa910b0fc01bc5c22201612b7d048f418e
kavitha0507 <kavithamynala@gmail.com>
2025-01-24 19:49:57 -0600

    commit

---
+++ src/main/java/model/Department.java
@@ -0,0 +1,11 @@
+package model;
+
+import java.util.ArrayList;
+
+public record Department() {
+    public void setDeptName(String development) {
+    }
+
+    public void setTeacherList(ArrayList<Teacher> teachersList) {
+    }
+}
